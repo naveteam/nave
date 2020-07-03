c0e870f9167a8fb8e91bceadc80ca7c0034e068f
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-07-03 17:34:55 -0300

    add first content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { ThemeProvider, Heading, Paragraph} from '@naveteam/ui-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nave.rs open JS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider>
+      <Heading>Nave OpenJS</Heading>
+      <Paragraph marginBottom='4'>Esse repositório terá como principal finalidade o desenvolvimento de qualquer desenvolvedor que queira se desenvolver no mundo JavaScript, consumindo o conteúdo provido pelo time de desenvolvedores da Nave.rs </Paragraph>
+      <Paragraph>Para isso, criaremos um repositório de conteúdos (em vídeo e em artigo) sobre as áreas de frontend, backend, devops e mobile, que podem ser consumidos para melhorar suas skills nesses assuntos.</Paragraph>
+      <Paragraph>Segue abaixo um link que baseará parte da stack que criaremos: <a href='https://github.com/kamranahmedse/developer-roadmap' target='_blank'>https://github.com/kamranahmedse/developer-roadmap</a></Paragraph>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
