733e23e5370bd455dff5fd2fd9a5d4df5ade7ecb
Eduardo Bittencourt <eduardo.bittencourt@nave.rs>
2020-07-03 17:34:38 -0300

    Updates

---
+++ precache-manifest.f1e62b019813ac22c7c71c79ef345975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaaaab136980b96225f60fbe7d66abba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75b00a47db40d890971e",
+    "url": "/static/js/2.bd7d6247.chunk.js"
+  },
+  {
+    "revision": "66b3acca1e1c31f0b14db86a1a3b5057",
+    "url": "/static/js/2.bd7d6247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6adc3633ce2fbb846ef7",
+    "url": "/static/js/main.d88975b0.chunk.js"
+  },
+  {
+    "revision": "987ece3b41ddc486c2fc",
+    "url": "/static/js/runtime-main.cb51d8f9.js"
+  }
+]);
